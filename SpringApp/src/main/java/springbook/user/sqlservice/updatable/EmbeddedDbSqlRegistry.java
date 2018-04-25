64ab04147c75cd14154b3da4813080f26c806a66
Hyewon Jo <hyewonjo92@gmail.com>
2018-04-25 19:00:02 +0900

    @Configuration 어노테이션 이용하여 Bean 등록 소스 작업 완료

--- SpringApp/src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ SpringApp/src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -8,7 +8,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
